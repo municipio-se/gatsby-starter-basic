df6b325052510b16f80a9c39d26444dfe6179446
Fredrik Johansson <jeanfredrik@gmail.com>
2021-08-20 14:13:06 +0200

    chore(deps): update all deps

--- src/fragments/WP_SearchSettingsForHook.js
+++ src/fragments/WP_SearchSettingsForHook.js
@@ -3,7 +3,7 @@ import { graphql } from "gatsby";
 export const query = graphql`
   fragment WP_SearchSettingsForHook on WP {
     search {
-      displaySettings {
+      searchDisplay {
         emptySearchResultMessage
         searchPlaceholderText
         searchLabelText
