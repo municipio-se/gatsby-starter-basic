5e5b5de5085437eb757ca777be73a08765674800
Fredrik Johansson <jeanfredrik@gmail.com>
2021-09-13 15:13:56 +0200

    chore(deps): update all deps

--- src/fragments/WP_ModularityAreaModules.js
+++ src/fragments/WP_ModularityAreaModules.js
@@ -17,33 +17,33 @@ export const query = graphql`
             name
           }
         }
-        # ... on WP_ModContacts {
-        #   ...WP_ModContacts
-        # }
-        # ... on WP_ModFileslist {
-        #   ...WP_ModFileslist
-        # }
-        # ... on WP_ModGallery {
-        #   ...WP_ModGallery
-        # }
-        # ... on WP_ModIframe {
-        #   ...WP_ModIframe
-        # }
-        # ... on WP_ModImage {
-        #   ...WP_ModImage
-        # }
-        # ... on WP_ModNotice {
-        #   ...WP_ModNotice
-        # }
-        # ... on WP_ModPosts {
-        #   ...WP_ModPosts
-        # }
+        ... on WP_ModContacts {
+          ...WP_ModContacts
+        }
+        ... on WP_ModFileslist {
+          ...WP_ModFileslist
+        }
+        ... on WP_ModGallery {
+          ...WP_ModGallery
+        }
+        ... on WP_ModIframe {
+          ...WP_ModIframe
+        }
+        ... on WP_ModImage {
+          ...WP_ModImage
+        }
+        ... on WP_ModNotice {
+          ...WP_ModNotice
+        }
+        ... on WP_ModPosts {
+          ...WP_ModPosts
+        }
         ... on WP_ModText {
           ...WP_ModText
         }
-        # ... on WP_ModTable {
-        #   ...WP_ModTable
-        # }
+        ... on WP_ModTable {
+          ...WP_ModTable
+        }
       }
     }
   }
