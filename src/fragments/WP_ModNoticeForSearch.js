c31602cb32ded4b1acacdbc1cfb4a7e06a118dee
Fredrik Johansson <jeanfredrik@gmail.com>
2021-12-21 13:40:06 +0100

    chore: update theme

--- src/fragments/WP_ModNoticeForSearch.js
+++ src/fragments/WP_ModNoticeForSearch.js
@@ -1,7 +1,7 @@
 import { graphql } from "gatsby";
 
 export const query = graphql`
-  fragment WP_ModTextForSearch on WP_ModText {
+  fragment WP_ModNoticeForSearch on WP_ModText {
     title
     hideTitle
     content
