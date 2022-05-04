26465c54583bb25dc0aa0cad7253188b7355378d
Fredrik Johansson <jeanfredrik@gmail.com>
2022-05-04 15:03:22 +0200

    fix: theme.js path

--- src/@municipio/gatsby-theme-basic/theme/index.js
+++ src/@municipio/gatsby-theme-basic/theme/index.js
@@ -1,7 +1,7 @@
 import parentTheme from "@municipio/gatsby-theme-basic/src/theme";
 import { mergeThemes } from "@whitespace/components";
 
-import childTheme from "../../theme";
+import childTheme from "../../../theme";
 
 var theme = mergeThemes(parentTheme, childTheme);
 
