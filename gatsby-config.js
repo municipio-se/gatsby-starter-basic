b03b13797b5a81c6002986b04d6eefe07ca732fb
Fredrik Johansson <jeanfredrik@gmail.com>
2022-06-02 17:45:09 +0200

    fix: Build error

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,10 @@ import { loadConfig, truey } from "@municipio/gatsby-theme-basic";
 loadConfig();
 
 export const siteMetadata = {
+  author: "Municipio",
+  description: `Starter site using Wordpress and Gatsby`,
   siteUrl: process.env.GATSBY_SITE_URL,
   title: `Wordpress Starter`,
-  description: `Starter site using Wordpress and Gatsby`,
 };
 
 export const plugins = [
@@ -23,6 +24,30 @@ export const plugins = [
         url: process.env.GATSBY_WORDPRESS_URL,
         refetchInterval: process.env.WORDPRESS_REFETCH_INTERVAL,
         nodesPerFetch: Number(process.env.WORDPRESS_NODES_PER_FETCH),
+      },
+      siteIndex: {
+        includePage: ({ page }) =>
+          page.context.contentType && page.context.contentType.name === "page",
+        localizations: {
+          en: {
+            basePath: "/content",
+            alphabet: Array.from("abcdefghijklmnopqrstuvwxyz"),
+            restInitial: {
+              path: "/other-pages",
+              title: "Other pages",
+              label: "#",
+            },
+          },
+          sv: {
+            basePath: "/innehall",
+            alphabet: Array.from("abcdefghijklmnopqrstuvwxyzåäö"),
+            restInitial: {
+              path: "/ovriga-sidor",
+              title: "Övriga sidor",
+              label: "#",
+            },
+          },
+        },
       },
       // XXX: postcss.config.js doesn’t seem to load automatically
       postCss: { postcssOptions: require("./postcss.config")() },
