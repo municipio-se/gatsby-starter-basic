70420816076ad74fd6a33ffdfed6c2019db228a6
Fredrik Johansson <jeanfredrik@gmail.com>
2023-04-03 08:35:39 +0200

    wip: Adapt to new theme version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,26 @@
-import { falsey, loadConfig, truey } from "@municipio/gatsby-theme-basic";
-import { createProxyMiddleware } from "http-proxy-middleware";
+const { falsey, loadConfig } = require("@municipio/gatsby-theme-basic");
+const { startOfDay, parseISO, format: formatDate } = require("date-fns");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const gql = require("tagged-template-noop");
 
-export const developMiddleware = (app) => {
+loadConfig();
+
+const algoliaUrl = new URL(
+  process.env.API_PROXY ||
+    process.env.GATSBY_SITE_URL ||
+    "http://localhost:3000",
+);
+
+const algoliaHosts = [
+  {
+    protocol: algoliaUrl.protocol.replace(":", ""),
+    url: `${algoliaUrl.host}/api/algolia`,
+  },
+];
+const algoliaIndexName = process.env.GATSBY_ALGOLIA_INDEX_NAME;
+const algoliaReplicas = [`${algoliaIndexName}_publish_date`];
+
+exports.developMiddleware = (app) => {
   if (process.env.API_PROXY) {
     app.use(
       "/api",
@@ -20,16 +39,16 @@ export const developMiddleware = (app) => {
   }
 };
 
-loadConfig();
-
-export const siteMetadata = {
+const siteMetadata = {
   author: "Municipio",
   description: `Starter site using Wordpress and Gatsby`,
   siteUrl: process.env.GATSBY_SITE_URL,
   title: `Municipio Starter`,
 };
 
-export const plugins = [
+exports.siteMetadata = siteMetadata;
+
+exports.plugins = [
   "gatsby-plugin-remove-serviceworker",
   {
     resolve: `@municipio/gatsby-theme-basic`,
@@ -41,29 +60,220 @@ export const plugins = [
         defaultLanguage: "sv",
         languages: ["sv", "en"],
       },
+      wsui: true,
       wp: {
         url: process.env.GATSBY_WORDPRESS_URL,
         refetchInterval: process.env.WORDPRESS_REFETCH_INTERVAL,
         nodesPerFetch: Number(process.env.WORDPRESS_NODES_PER_FETCH),
       },
-      search: {
-        paths: ["sok", "en/search"],
-      },
       disableSearchPlugin: false,
+      search: {
+        paths: {
+          "en/search": {
+            context: {
+              title: "Search",
+            },
+          },
+          sok: {
+            context: {
+              title: "Sök",
+            },
+          },
+        },
+        // archives: {
+        //   post: {
+        //     searchBox: true,
+        //     facets: {
+        //       dates: false,
+        //     },
+        //   },
+        // },
+        algolia: {
+          appId: process.env.GATSBY_ALGOLIA_APP_ID,
+          // Use Admin API key without GATSBY_ prefix, so that the key isn't exposed in the application
+          // Tip: use Search API key with GATSBY_ prefix to access the service from within components
+          apiKey: process.env.ALGOLIA_ADMIN_KEY,
+          searchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+          indexName: algoliaIndexName, // for all queries
+          queries: [
+            {
+              query: gql`
+                query {
+                  pages: allSitePage(
+                    filter: { context: { isIncludedInSearch: { eq: true } } }
+                  ) {
+                    nodes {
+                      id #required by gatsby-plugin-algolia
+                      path
+                      context {
+                        title
+                        textContent
+                        language
+                        publishDate: dateGmt
+                        dates: archiveDates
+                        modified: modifiedGmt
+                        image: featuredImage {
+                          node {
+                            databaseId
+                            base64
+                            src
+                            srcSet
+                            srcWebp
+                            srcSetWebp
+                            width
+                            height
+                            alt
+                            # caption
+                            # credit
+                          }
+                        }
+                        contentType {
+                          name
+                        }
+                      }
+                    }
+                  }
+                  pdfs: wp {
+                    mediaItems(where: { mimeType: APPLICATION_PDF }) {
+                      nodes {
+                        id
+                        title
+                        publishDate: dateGmt
+                        dates: archiveDates
+                        modified: modifiedGmt
+                        mediaItemUrl
+
+                        # For thumbnail:
+                        databaseId
+                        base64: base64Uri
+                        src: sourceUrl(size: WIDE_LARGE)
+                        srcSet: srcSet(size: WIDE_LARGE)
+                        srcWebp: sourceUrl(size: WIDE_LARGE)
+                        srcSetWebp: srcSet(size: WIDE_LARGE)
+                        width(size: WIDE_LARGE)
+                        height(size: WIDE_LARGE)
+                        alt: altText
+                        caption
+                        credit
+                      }
+                    }
+                  }
+                }
+              `,
+              queryVariables: {}, // optional. Allows you to use graphql query variables in the query
+              transformer: ({ data }) => {
+                // console.log(data.pages.nodes.map((page) => page.path));
+                return [
+                  ...data.pages.nodes.map(
+                    ({ context: { ...context }, ...page }) => {
+                      return {
+                        ...page,
+                        ...context,
+                        image: context.image && context.image.node,
+                        dates:
+                          context.dates &&
+                          context.dates.map((date) => {
+                            let dateObj = startOfDay(parseISO(date));
+                            return {
+                              formatted: formatDate(dateObj, "yyyy-MM-dd"),
+                              numeric: dateObj.valueOf(),
+                            };
+                          }),
+                      };
+                    },
+                  ),
+                  ...data.pdfs.mediaItems.nodes.map(
+                    ({
+                      dates,
+                      mediaItemUrl,
+                      databaseId,
+                      base64,
+                      src,
+                      srcSet,
+                      srcWebp,
+                      srcSetWebp,
+                      width,
+                      height,
+                      alt,
+                      ...attributes
+                    }) => {
+                      return {
+                        ...attributes,
+                        language: attributes.language || "sv",
+                        contentType: {
+                          name: "file",
+                        },
+                        file: {
+                          url: mediaItemUrl,
+                        },
+                        dates: (dates || []).map((date) => {
+                          let dateObj = startOfDay(parseISO(date));
+                          return {
+                            formatted: formatDate(dateObj, "yyyy-MM-dd"),
+                            numeric: dateObj.valueOf(),
+                          };
+                        }),
+                        image: src
+                          ? {
+                              databaseId,
+                              base64,
+                              src,
+                              srcSet,
+                              srcWebp,
+                              srcSetWebp,
+                              width,
+                              height,
+                              alt,
+                            }
+                          : undefined,
+                      };
+                    },
+                  ),
+                ];
+              }, // optional
+              // indexName: "", // overrides main index name, optional
+              // settings: {
+              //   // optional, any index settings
+              //   // Note: by supplying settings, you will overwrite all existing settings on the index
+              // },
+              // mergeSettings: false, // optional, defaults to false. See notes on mergeSettings below
+            },
+          ],
+          // chunkSize: 10000, // default: 1000
+          settings: {
+            // optional, any index settings
+            // Note: by supplying settings, you will overwrite all existing settings on the index
+            searchableAttributes: [
+              "title",
+              "textContent",
+              "file.attachment.content",
+            ],
+            attributesToHighlight: [
+              "title",
+              "textContent",
+              "file.attachment.content",
+            ],
+            attachmentAttributes: ["file"],
+            attributesForFaceting: ["contentType.name"],
+            unretrievableAttributes: ["internal"],
+            numericAttributesForFiltering: ["dates.numeric"],
+            replicas: algoliaReplicas,
+            hitsPerPage: 24,
+          },
+          // mergeSettings: false, // optional, defaults to false. See notes on mergeSettings below
+          // concurrentQueries: false, // default: true
+          // dryRun: false, // default: false, only calculate which objects would be indexed, but do not push to Algolia
+          // continueOnFailure: false, // default: false, don't fail the build if Algolia indexing fails
+          algoliasearchOptions: {
+            hosts: algoliaHosts,
+          }, // default: { timeouts: { connect: 1, read: 30, write: 30 } }, pass any different options to the algoliasearch constructor
+        },
+      },
       disableDefaultArchivePages: false,
       siteIndex: {
         includePage: ({ page }) =>
           page.context.contentType && page.context.contentType.name === "page",
         localizations: {
-          sv: {
-            basePath: "/innehall",
-            alphabet: Array.from("abcdefghijklmnopqrstuvwxyzåäö"),
-            restInitial: {
-              path: "/övriga-sidor",
-              title: "Övriga sidor",
-              label: "#",
-            },
-          },
           en: {
             basePath: "/content",
             alphabet: Array.from("abcdefghijklmnopqrstuvwxyz"),
--- src/fragments/WP_PostForPage.js
+++ src/fragments/WP_PostForPage.js
@@ -2,13 +2,6 @@ import { graphql } from "gatsby";
 
 export const query = graphql`
   fragment WP_PostForPage on WP_Post {
-    tags {
-      nodes {
-        name
-        id
-        uri
-      }
-    }
     modifiedGmt
   }
 `;
