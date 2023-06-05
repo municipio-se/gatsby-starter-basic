cf8033efa7a2fa4bc6463785d6b0df662fe69a6d
Fredrik Johansson <jeanfredrik@gmail.com>
2023-06-05 12:45:08 +0200

    feat: Change default font

--- src/wsui/theme.jsx
+++ src/wsui/theme.jsx
@@ -1,12 +1,20 @@
 import { mergeThemes } from "@wsui/base";
-import theme from "@wsui/theme-standard";
+import theme from "@wsui/theme-minimal";
 
 import "./index.css";
 
 export default mergeThemes(theme, {
   typography: {
     fonts: {
-      Inter: "/fonts/Inter.var.woff2",
+      // Inter: "/fonts/Inter.var.woff2",
+      "Open Sans": {
+        src: {
+          normal:
+            'url("/fonts/OpenSans-VariableFont_wdth,wght.ttf") format("truetype")',
+          italic:
+            'url("/fonts/OpenSans-Italix-VariableFont_wdth,wght.ttf") format("truetype")',
+        },
+      },
     },
   },
 });
